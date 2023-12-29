0b4bb5096719b579e30d3f4530e561d73ac8a7bf
Amina Mohamed <amishmohamed2002@gmail.com>
2023-12-30 00:34:22 +0300

    Added an api key

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+function searchCity(){
+
+  let apiKey="9a708b23a8ct6o0b4d94fd3134a86038"
+  // let apiUrl =`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+  console.log(apiUrl);
+}
+
+
 // receives an event
 function submitButton(event){
   // this prevents from changes in the webpage even if the you reload the web page
